56cddd4ff62cbbb026416877ca3db43733eda609
Josep <jbatalle3@gmail.com>
2015-01-05 17:00:05 +0100

    MQnaas resource templates -> centralized function

--- src/main/webapp/js/mqnaasTemplates.js
+++ src/main/webapp/js/mqnaasTemplates.js
@@ -1,5 +1,5 @@
-getTSON = function(endpoint){
-    return '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:rootResourceDescriptor xmlns:ns2="org.mqnaas"><specification><type>TSON</type><model>Internal</model><version>1.0</version></specification><endpoints><endpoint><uri>'+endpoint+'</uri></endpoint></endpoints></ns2:rootResourceDescriptor>';
+getResource = function(resourceType, endpoint){
+    return '<?xml version="1.0" encoding="UTF-8" standalone="yes"?><ns2:rootResourceDescriptor xmlns:ns2="org.mqnaas"><specification><type>'+resourceType+'</type><model>Internal</model><version>1.0</version></specification><endpoints><endpoint><uri>'+endpoint+'</uri></endpoint></endpoints></ns2:rootResourceDescriptor>';
 };
 
 getARN = function(endpoint){
