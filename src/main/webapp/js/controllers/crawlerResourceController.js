af706255ac7b6c9f12d28868a4266bebdcf93830
Josep <jbatalle3@gmail.com>
2015-01-06 12:40:09 +0100

    Changes in crawler. PRoblems with localstorage(json) and treeModel

--- src/main/webapp/js/controllers/crawlerResourceController.js
+++ src/main/webapp/js/controllers/crawlerResourceController.js
@@ -6,50 +6,59 @@ angular.module('openNaaSApp')
             var url = "";
             var childNode;
             var tree;
-localStorageService.set("mqNaaSElements", "");
+            localStorageService.set("mqNaaSElements", "");
             var getRootResources = function () {
                 RootResourceService.list().then(function (data) {
-                    if (data.IRootResource.IRootResourceId instanceof Array) {
-                        data = data.IRootResource.IRootResourceId;
-                    } else {
-                        data = [data.IRootResource.IRootResourceId];
-                    }
+                    data = checkIfIsArray(data.IRootResource.IRootResourceId);
 
                     console.log(data);
                     tree = new TreeModel();
-                    var root = tree.parse({name: 'rootResource', children: [{}]});
+                    var root = tree.parse({name: 'rootResource', children: []});
+                    console.log(root);
+                    console.log(JSON.stringify(root, ["name", "children", "model"]));
+//                    console.log(jsonify(root));
+//                    localStorageService.set("mqNaaSElements", root);
+//                    return;
+
                     console.log(data.length);
                     for (var i = 0; i < data.length; i++) {
                         console.log(data[i]);
                         childNode = root.addChild(tree.parse({name: data[i]}));
+                        console.log(childNode);
                         url = generateUrl("IRootResourceAdministration", data[i], "IRootResourceProvider");
-                        getMqNaaSResource(url);
+                        getMqNaaSResource(root, data[i], url);
                     }
                     console.log(root);
+//                    console.log(JSON.stringify(root, ["name", "children", "name", "children", "model"]));
+//                    console.log(JSON.stringify(root, undefined, ""));
+console.log(cloneSO(root));
+console.log(JSON.stringify(root.model));
+                    console.log(jsonify(root));
                     //                var url = generateUrl("IRootResourceAdministration", $routeParams.id, "IRootResourceProvider");
 
-                    $scope.data = data;
-                    localStorageService.set("mqNaaSElements", root);
+//                    $scope.data = data;
+//                    localStorageService.set("mqNaaSElements", root);
                 });
             };
 
-
-            var getMqNaaSResource = function (url) {
-                console.log("MQNAAS FUNCTION");
+            var getMqNaaSResource = function (root, parent, url) {
+                console.log(root);
+                console.log(parent);
                 MqNaaSResourceService.list(url).then(function (data) {
-                    if(data === undefined) return;
-                    if (data.IRootResource.IRootResourceId instanceof Array) {
-                        data = data.IRootResource.IRootResourceId;
-                    } else {
-                        data = [data.IRootResource.IRootResourceId];
-                    }
+                    console.log(data);
+                    if (data === undefined)
+                        return;
+                    data = checkIfIsArray(data.IRootResource.IRootResourceId);
                     for (var i = 0; i < data.length; i++) {
                         console.log(data[i]);
-                        childNode.addChild(tree.parse({name: data[i]}));
+                        var n = root.first(function (node) {
+                            return node.model.name === parent;
+                        });
+                        n.addChild(tree.parse({name: data[i]}));
                         url = generateUrl("IRootResourceAdministration", data[i], "IRootResourceProvider");
-                        getMqNaaSResource(url);
+                        getMqNaaSResource(root, data[i], url);
                     }
-                }, function(error){
+                }, function (error) {
                     console.log(error);
                 });
             };
