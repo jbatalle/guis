b6833036a6794dbff05f62803b7122b5f974071c
Josep <jbatalle3@gmail.com>
2015-01-08 19:20:14 +0100

    Create VI controller. Views, graphs and dialogs

--- src/main/webapp/js/controllers/createVIController.js
+++ src/main/webapp/js/controllers/createVIController.js
@@ -1,9 +1,10 @@
 'use strict';
 
 angular.module('openNaaSApp')
-        .controller('listVIController', function ($scope, MqNaaSResourceService, $filter, ngTableParams) {
+        .controller('listVIController', function ($scope, $rootScope, MqNaaSResourceService, $filter, ngTableParams, viService, localStorageService) {
             console.log("LIST VI");
-            var urlListVI = "IRootResourceAdministration/Network-Internal-1.0-2/IRequestManagement";
+    $rootScope.networkId = "Network-Internal-1.0-2";
+            var urlListVI = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement";
             $scope.data = [];
             MqNaaSResourceService.list(urlListVI).then(function (result) {
                 console.log(result);
@@ -28,27 +29,135 @@ angular.module('openNaaSApp')
             });
 
             $scope.createVIRequest = function () {
-                var urlCreateVI = "IRootResourceAdministration/Network-Internal-1.0-2/IRequestManagement";
+                var urlCreateVI = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement";
                 MqNaaSResourceService.put(urlCreateVI).then(function (result) {
                     $scope.data.push(result);
+                    var vi = {"name": result};
+                    console.log("CREATE VI AND save spring...");
+                    viService.createVI(vi);
+                    localStorageService.set("virtualElements", []);
                     $scope.tableParams.reload();
                 });
             };
+            $scope.deleteVIRequest = function (viReq) {
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+viReq;
+                MqNaaSResourceService.remove(url).then(function (result) {
+//                    viService.createVI(vi);
+                    $scope.tableParams.reload();
+                });
+            };
+            
+            $scope.sendVIR = function (viReq){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestBasedNetworkManagement/?arg0="+viReq;
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.resRoot = result;//empty
+                });
+            };
+            
         })
-        .controller('editVIController', function ($scope, MqNaaSResourceService, $routeParams) {
+        .controller('editVIController', function ($scope, $rootScope, MqNaaSResourceService, $routeParams, ngDialog, viService, localStorageService) {
             console.log("Edit VI : " + $routeParams.id);
             $scope.viId = $routeParams.id;
-
-            var urlPeriod = "IRootResourceAdministration/Network-Internal-1.0-2/IRequestManagement/" + $scope.viId + "/IRequestAdministration/period";
+$scope.virtualPort = [];
+            var urlPeriod = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/" + $scope.viId + "/IRequestAdministration/period";
             MqNaaSResourceService.get(urlPeriod).then(function (result) {
                 $scope.period = result;
+                $scope.period.startDate = "1/8/15";
+                console.log(result);
+                var d = new Date(result.startDate+1000);
+                console.log(d);
+//                console.log(formatDate(d));
+                console.log(customFormat( "#DD#/#MM#/#YYYY#", d));
             });
 
-            $scope.setPeriod = function (start, end) {
-                var urlPeriod = "IRootResourceAdministration/Network-Internal-1.0-2/IRequestManagement/" + $scope.viId + "/IRequestAdministration/period";
-                var period = getPeriod(949359600, 980895600);//xml
-                MqNaaSResourceService.put(urlPeriod, period).then(function () {//the response is empty
+            $scope.setPeriod = function (period) {
+                console.log(period);
+                console.log(new Date(period.startDate).getTime());
+                var urlPeriod = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/" + $scope.viId + "/IRequestAdministration/period";
+                var onPeriod = getPeriod(new Date(period.startDate).getTime()/1000, new Date(period.endDate).getTime()/1000);//xml
+                MqNaaSResourceService.put(urlPeriod, onPeriod).then(function () {//the response is empty
+                    
+                });
+            };
+            $scope.addResourceToVI = function (resourceType){//resourceType TSON/ARN/CPE...
+                console.log("ADD RESOURCE TO VI");
+                resourceType = resourceType.toUpperCase();
+                console.log(resourceType);
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+$scope.viId+"/IRequestResourceManagement/?arg0="+resourceType;
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.resourceRequest = result;
+                    viService.addResourceToVI($scope.viId, result, resourceType);
+                    var vEl = localStorageService.get("virtualElements");
+                    vEl.push({name: result, type: resourceType});
+                    localStorageService.set("virtualElements", vEl);
+                });
+            };
+            $scope.addVirtualPortToResource = function (resourceRequest){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+$scope.viId+"/IRequestResourceManagement/"+resourceRequest+"/IPortManagement";
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.virtualPort.push(result);
+                });
+            };
+            $scope.mapVirtualResourceToRealResource = function (resourceRequest, realResource){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+$scope.viId+"/IRequestResourceManagement/"+resourceRequest+"/IRequestResourceMapping/defineMapping/?arg0="+resourceRequest+"&arg1="+realResource;
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.resRoot = result;//empty
+                });
+            };
+            $scope.mapVirtualPorts = function (virtualPort, realPort){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+$scope.viId+"/IRequestResourceMapping/defineMapping/?arg0="+virtualPort+"&arg1="+realPort;
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.resRoot = result;//empty
+                });
+            };
+            $scope.sendVIR = function (viReq){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestBasedNetworkManagement/?arg0="+viReq;
+                MqNaaSResourceService.put(url).then(function (result) {
+                    $scope.resRoot = result;//empty
+                });
+            };
+            $scope.addResourceToGraph = function(name){
+                console.log($scope.ngDialogData);
+                console.log(name);
+                createElement(name, $scope.ngDialogData.nodeType, $scope.ngDialogData.divPos);
+                ngDialog.close();
+            };
+            
+            $scope.getVirtualPorts = function(virtualRes){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRequestManagement/"+$scope.viId+"/IRequestResourceManagement/"+virtualRes+"/IPortManagement";
+                MqNaaSResourceService.get(url).then(function (result) {
+                    $scope.virtualPorts = result;
+                });
+            };
+            $scope.getPhysicalPorts = function(resourceName){
+                var url = "IRootResourceAdministration/"+$rootScope.networkId+"/IRootResourceAdministration/"+resourceName+"/IPortManagement";
+                MqNaaSResourceService.get(url).then(function (result) {
+                    $scope.physicalPorts = result;
                 });
             };
 
         });
+        
+        
+        
+        
+function customFormat(formatString, d){
+    var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+    var dateObject = d;
+    YY = ((YYYY=dateObject.getFullYear())+"").slice(-2);
+    MM = (M=dateObject.getMonth()+1)<10?('0'+M):M;
+    MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+    DD = (D=dateObject.getDate())<10?('0'+D):D;
+    DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObject.getDay()]).substring(0,3);
+    th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+    formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+
+    h=(hhh=dateObject.getHours());
+    if (h==0) h=24;
+    if (h>12) h-=12;
+    hh = h<10?('0'+h):h;
+    AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+    mm=(m=dateObject.getMinutes())<10?('0'+m):m;
+    ss=(s=dateObject.getSeconds())<10?('0'+s):s;
+    return formatString.replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+}
--- src/main/webapp/js/directives/graph.js
+++ src/main/webapp/js/directives/graph.js
@@ -269,4 +269,163 @@ angular.module('openNaaSApp')
                             createElement(nodes[i].id, nodes[i].type, divPos, data);
                         }
                     }};
-            }]);
+            }])
+        .directive('droppablevi', ['$window', '$rootScope', 'localStorageService', '$timeout', function ($window, $rootScope, localStorageService, timer) {
+                return {
+                    // A = attribute, E = Element, C = Class and M = HTML Comment
+                    restrict: 'AE',
+                    controller: function ($scope, ngDialog) {
+                        console.log($scope);
+                        $scope.openARNDialog = function (nodeType, divPos) {
+                            ngDialog.open({
+                                template: 'partials/sodales/arnVIDialog.html',
+                                data: {"nodeType": nodeType, "divPos": divPos}})
+                        };
+                        $scope.openCPEDialog = function (nodeType, divPos) {
+                            ngDialog.open({
+                                template: 'partials/sodales/cpeVIDialog.html',
+                                data: {"nodeType": nodeType, "divPos": divPos}});
+                        };
+                    },
+                    link: function (scope, element, attrs) {
+
+                        element.droppable({
+                            drop: function (e, ui) {
+                                var $newPosX = ui.offset.left - $(this).offset().left;
+                                var $newPosY = ui.offset.top - $(this).offset().top;
+                                var nodeType = angular.element(ui.draggable).parent().context.id;
+                                var divPos = {};
+                                var $div = $("#graph");
+                                //    var e = window.event;
+                                divPos = {//position where the element is dropped
+                                    x: $newPosX,
+                                    y: $newPosY
+                                };
+
+                                console.log("Create with draw");
+                                console.log(graph.getNodes());
+                                if (nodeType === "arn") {
+                                    scope.openARNDialog(nodeType, divPos);
+                                } else if (nodeType === "cpe") {
+                                    scope.openCPEDialog(nodeType, divPos);
+                                }
+//                                createElement("randomName", nodeType, divPos);
+
+                                var dragIndex = angular.element(ui.draggable).data('index'),
+                                        reject = angular.element(ui.draggable).data('reject'),
+                                        dragEl = angular.element(ui.draggable).parent(),
+                                        dropEl = angular.element(this);
+                                if (dragEl.hasClass('list1') && !dropEl.hasClass('list1') && reject !== true) {
+                                    scope.list2.push(scope.list1[dragIndex]);
+                                    scope.list1.splice(dragIndex, 1);
+                                } else if (dragEl.hasClass('list2') && !dropEl.hasClass('list2') && reject !== true) {
+                                    scope.list1.push(scope.list2[dragIndex]);
+                                    scope.list2.splice(dragIndex, 1);
+                                }
+                                scope.$apply();
+                            }
+                        });
+                    }
+                };
+            }])
+        .directive('grapheditorvi', ['localStorageService', '$timeout', function (localStorageService, timer) {
+                return {
+                    restrict: 'EA',
+                    scope: {},
+                    templateUrl: 'partials/d3/editorVI.html',
+                    link: function (scope, element, attrs) {
+                        graph = new myGraph("#graph");
+                        graph.setNodes(localStorageService.get("graphNodes"));
+                        scope.graph = graph;
+                        timer(console.log("TIMER HEUEHE"), 0);
+                        console.log(localStorageService.get("graphNodes"));
+                        console.log(localStorageService.get("networkElements"));
+                        console.log(localStorageService.get("virtualElements"));
+
+                        var networkElements = localStorageService.get("networkElements");
+                        var virtualElements = localStorageService.get("virtualElements");
+console.log(networkElements);
+console.log(virtualElements);
+networkElements = networkElements.concat(virtualElements).filter( function(el) {
+    return el !== null;
+});
+console.log(networkElements);
+                        var links = [];
+                        var lin = {s: 0, t: 1};
+                        links.push(lin);
+                        var nodes = [];
+                        //for each network element
+                        for (var i = 0; i < networkElements.length; i++) {
+                            console.log(networkElements[i]);
+                            //IF IS VIRTUAL TAKE THE VALUE FROM THE RESOURCE TYPE JSON
+                            var type = networkElements[i].split("-")[0].toLowerCase();
+                            var id = networkElements[i];
+//                        var ports = networkElements[i].ports.port;
+                            var ports = [];
+//        createSwitch(id, ports);
+                            var node = {};
+                            node.id = id;
+                            node.type = type;
+                            node.ports = ports;
+                            node.x = Math.floor((Math.random() * 400) + 1);
+                            node.y = Math.floor((Math.random() * 400) + 1);
+                            node.net_force = {};
+                            node.velocity = {x: 0, y: 0};
+                            nodes.push(node);
+                            //{id:1, x: 10, y: 20, net_force: {}, velocity: {}
+                        }
+                        var edges = [];
+                        var edge = {};
+                        for (var i = 0; i < links.length; i++) {
+                            edge = {};
+                            for (var j = 0; j < nodes.length; j++) {
+                                for (var t = 0; t < nodes[j].ports.length; t++) {
+                                    if (nodes[j].ports[t]._id === links[i].srcPort) {
+                                        var srcP = j;
+                                    }
+                                    if (nodes[j].ports[t]._id === links[i].dstPort) {
+                                        var dstP = j;
+                                    }
+                                }
+                            }
+                            edge = {s: srcP, t: dstP};
+                            edges.push(edge);
+                        }
+
+                        //create edges
+                        var matrix = [];
+                        for (var i = 0; i < nodes.length; i++) {
+                            matrix[i] = [];
+                            for (var j = 0; j < nodes.length; j++) {
+                                matrix[i][j] = false;
+                            }
+                        }
+                        /*                    for (var i = 0; i < edges.length; i++) {
+                         matrix[edges[i].s][edges[i].t] = true;
+                         matrix[edges[i].t][edges[i].s] = true;
+                         }
+                         */
+                        nodes = StaticForcealgorithm(nodes, matrix);
+                        for (i = 0; i < nodes.length; i++) {
+//                            console.log(nodes[i].x + " " + nodes[i].y);
+                            if (nodes[i].x < 0) {
+                                nodes[i].x = nodes[i].x + 400 / 2;
+                            }
+                            if (nodes[i].y < 0) {
+                                nodes[i].y = nodes[i].y + 400 / 2;
+                            }
+                            if (nodes[i].x > 400) {
+                                nodes[i].x = 400;
+                            }
+                            if (nodes[i].y > 400) {
+                                nodes[i].y = 400;
+                            }
+//        createSwitch(nodes[i].id, nodes[i].ports, nodes[i].x, nodes[i].y);
+                            var divPos = {x: nodes[i].x, y: nodes[i].y};
+                            var data = {id: nodes[i].id, ports: nodes[i].ports};
+                            createElement(nodes[i].id, nodes[i].type, divPos, data);
+//                            localStorageService.set("graphNodes", graph.getNodes());
+                        }
+                    }};
+            }])
+        ;
