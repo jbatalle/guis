2029a98d54977e5a0dd75dc44383ed38bcd10fd1
Josep Batallé <jbatalle3@gmail.com>
2015-09-16 14:09:23 +0200

    Heartbeat in home ctrl

--- sodales/app/scripts/controllers/sodales/0_home.js
+++ sodales/app/scripts/controllers/sodales/0_home.js
@@ -1,22 +1,59 @@
 'use strict';
 
 angular.module('mqnaasApp')
-    .controller('SodalesHomeCtrl', function ($scope, $rootScope, HistoryService, spService, viService, MqNaaSResourceService) {
+    .controller('SodalesHomeCtrl', function ($scope, $rootScope, HistoryService, spService, viService, MqNaaSResourceService, $modal, $alert) {
+
+        //heartbeat mqnaas
+        var urlListVI = 'IRootResourceProvider';
+        MqNaaSResourceService.list(urlListVI).then(function (result) {
+            console.log(result);
+            if (result === undefined) {
+                $scope.alert.title = "Opennaas is offline!";
+                $scope.alert.content = "No connection with the machine.";
+                $scope.alert.type = 'danger';
+                myAlert.show();
+            }
+        }, function (error) {
+            myAlert.show();
+            console.log("test")
+            console.log(error);
+        });
+
+        $scope.alert = {
+            title: 'Opennaas is offline!',
+            content: 'No connection with the machine.',
+            type: 'danger'
+        };
+
+        // Service usage
+        var myAlert = $alert({
+            title: $scope.alert.title,
+            content: $scope.alert.content,
+            placement: 'top',
+            type: $scope.alert.type,
+            keyboard: true,
+            show: false,
+            container: '#alerts-container'
+        });
+
 
         spService.getList().then(function (data) {
             if (data) $scope.spSize = data.length;
             else $scope.spSize = 0;
         });
 
-        var urlListVI = "IRootResourceAdministration/" + $rootScope.networkId + "/IRequestManagement";
-        MqNaaSResourceService.list(urlListVI).then(function (result) {
-            console.log(result);
-            if (result === undefined) $scope.viSize = 0;
-            else {
-                var data = checkIfIsArray(result.IResource.IResourceId);
-                $scope.viSize = data.length;
-            }
-        });
+        if ($rootScope.networkId === undefined) $scope.viSize = 0;
+        else {
+            var urlListVI = "IRootResourceAdministration/" + $rootScope.networkId + "/IRequestManagement";
+            MqNaaSResourceService.list(urlListVI).then(function (result) {
+                console.log(result);
+                if (result === undefined) $scope.viSize = 0;
+                else {
+                    var data = checkIfIsArray(result.IResource.IResourceId);
+                    $scope.viSize = data.length;
+                }
+            });
+        }
 
         $scope.slicesSize = 0;
         /*            viService.list().then(function (data) {
