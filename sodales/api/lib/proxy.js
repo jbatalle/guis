9404440782b3efc626c09ac1ef9fbca48f9b47c3
Josep Batallé <jbatalle3@gmail.com>
2015-10-30 10:14:32 +0100

    Fix proxy error when getText

--- sodales/api/lib/proxy.js
+++ sodales/api/lib/proxy.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
     app.use('/rest/mqnaas', function (req, res) {
         console.log(req.method + ": " + req.url);
         var url = app.mqnaas + req.url;
-//        var url = 'http://84.88.40.174:9000/mqnaas' + req.url;
+        //        var url = 'http://84.88.40.174:9000/mqnaas' + req.url;
         console.log(url);
         //console.log(app.mqnaas + req.url);
         var r = null;
@@ -40,12 +40,11 @@ module.exports = function (app) {
             }).pipe(res);
         } else {
             request({
-                url: url ,
+                url: url,
                 headers: {
-                    accept: req.headers.accept,
-                    
+
                 }
-                
+
             }).pipe(res);
             //r = request(url);
             //r.pipe(request(url)).pipe(res) /*.setTimeout(2000)*/ ;
