20f936ece0c826f0c2f2073e27f179a10585d94e
Josep Batallé <jbatalle3@gmail.com>
2015-11-11 18:44:44 +0100

    Included getAllInterfaces

--- sodales/app/scripts/arn.js
+++ sodales/app/scripts/arn.js
@@ -2,6 +2,10 @@ getCards = function () {
     return '<?xml version="1.0" encoding="UTF-8"?><request><operation token="1" type="show" entity="card"><card equipmentId="0" _id="0"/></operation></request>';
 };
 
+getAllInterfaces = function () {
+    return '<?xml version="1.0" encoding="utf-8"?><request><operation token="1" type="show" entity="interface"><interface equipmentId="0"/></operation></request>';
+};
+
 getCardInterfaces = function (cardId) {
     return '<?xml version="1.0" encoding="UTF-8"?><request><operation token="1" type="show" entity="all"><interface equipmentId="0" cardId="' + cardId + '"/></operation></request>';
 };
