ca5024da6ba81067c479bc4ede592c8d538baf59
Josep Batallé <jbatalle3@gmail.com>
2015-12-17 18:27:53 +0100

    Removed unused migration

--- mod_authentication/db/migrate/20150810111831_create_vi_resource.rb
+++
@@ -1,8 +0,0 @@
-class CreateViResource < ActiveRecord::Migration
-  def change
-    create_table :vi_resources do |t|
-      t.string :name, :unique => true
-      t.string :type
-    end
-  end
-end
