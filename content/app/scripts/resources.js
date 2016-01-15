4a93a91b552aae635e8d68378c1f25da5ae82270
Josep Batallé <jbatalle3@gmail.com>
2016-01-15 14:51:30 +0100

    Topology files

--- content/app/scripts/resources.js
+++ content/app/scripts/resources.js
@@ -4,17 +4,44 @@ var resources_list = {
         {
             id: "Tson-24",
             type: "tson",
-            endpoint: "http://137.222.177.116:8080"
+            endpoint: "http://137.222.177.116:8080",
+            ports: [
+                {
+                    id: "T11"
+                }, {
+                    id: "T12"
+                }, {
+                    id: "T13"
+                }
+            ]
         },
         {
             id: "Tson-26",
             type: "tson",
-            endpoint: "http://137.222.177.117:8080"
+            endpoint: "http://137.222.177.117:8080",
+            ports: [
+                {
+                    id: "T11"
+                }, {
+                    id: "T12"
+                }, {
+                    id: "T13"
+                }
+            ]
         },
         {
             id: "Tson-25",
             type: "tson",
-            endpoint: "http://137.222.177.118:8080"
+            endpoint: "http://137.222.177.118:8080",
+            ports: [
+                {
+                    id: "T11"
+                }, {
+                    id: "T12"
+                }, {
+                    id: "T13"
+                }
+            ]
         },
         {
             id: "nitos-network",
